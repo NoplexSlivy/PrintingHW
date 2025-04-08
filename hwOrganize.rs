aa71cc3672245a2e7984f43e74f5c17f4e857ae5
NoplexSlivy <134464820+NoplexSlivy@users.noreply.github.com>
2025-04-08 13:12:03 -0500

    classes

--- hwOrganize.rs
+++ hwOrganize.rs
@@ -1,4 +1,4 @@
-enum Class{
+enum Classes{
     Math,
     English,
     Science,
@@ -6,42 +6,37 @@ enum Class{
 }
 
 struct Homework{
-    class: Class,
+    class: Classes,
     assignment: String,
     daysdue: usize,
 }
 
 fn main(){
-    let hw1 = Homework {
-        class: Class::Math,
+    let hw1 = &Homework {
+        class: Classes::Math,
         assignment: String::from("Algebra"),
         daysdue: 3,
     };
 
-    let hw2 = Homework {
-        class: Class::English,
+    let hw2 = &Homework {
+        class: Classes::English,
         assignment: String::from("Essay"),
         daysdue: 7,
     };
 
-    let hw3 = Homework {
-        class: Class::Science,
+    let hw3 = &Homework {
+        class: Classes::Science,
         assignment: String::from("Lab Report"),
         daysdue: 5,
     };
 
-    let hw4 = Homework {
-        class: Class::History,
+    let hw4 = &Homework {
+        class: Classes::History,
         assignment: String::from("Research Paper"),
         daysdue: 10,
     };
-
-
-    let homW = vec![hw1,hw2,hw3,hw4];
+    let homW = vec![&hw1,&hw2,&hw3,&hw4];
     for x in homW{
-
         println!("the {} assignment, in {} class, is due in {} days" , x.assignment, x.class, x.daysdue); 
     }
-    
-
 }
